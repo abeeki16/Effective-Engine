6a4060076568ff003e2d9fc9211bad8e414cb4a0
User-PC\User <sollozzo@bu.edu>
2018-11-11 17:14:48 -0500

    updating all in one

--- readmatreal.c
+++ readmatreal.c
@@ -1,15 +1,125 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+key_t keya;
+int shmBuf1id;
+int (*shmptra)[10];
+
+key_t keyb;
+int shmBuf2id;
+int (*shmptrb)[10];
+
+key_t keyc;
+int shmBuf3id;
+int (*shmptrc)[10];
+
+int mata[10][10];
+int matb[10][10];
+int arowcount = 0;
+int acolcount = 0;
+int browcount = 0;
+int bcolcount = 0;
+
+void createBuf1() {	
+	char * argv[7];
+	//will need more strids for other matrices
+	char strida[20];
+	char stridb[20];
+	char stridc[20];
+	pid_t pid;
+	//making matrix a
+	keya = ftok(".",'a');
+	shmBuf1id = shmget(keya,sizeof(int[10][10]),IPC_CREAT|0666);
+	if(shmBuf1id == -1 ) {  
+		perror("shmget error");
+		exit(1);
+	}
+	
+	printf("Don't look! I'm shmatting\n");
+	shmptra = shmat(shmBuf1id,0,0);
+	if(shmptra == (void*) -1 ){  
+		perror("shmat error");
+		exit(1);
+	}
+	/*shmptra[0][0] = 1;
+	printf("%d\n",shmptra[0][0]);
+	printf("%p\n",shmptra); */
+
+	//making space for matrix b
+	keyb = ftok(".",5982);
+	shmBuf2id = shmget(keyb,sizeof(int[10][10]),IPC_CREAT|0666);
+	if(shmBuf2id == -1) {
+		perror("shmget b error");
+		exit(1);
+	}
+	shmptrb = shmat(shmBuf2id,0,0);
+	if (shmptrb == (void*) -1) {
+		perror("shmat b error");
+		exit(1);
+	}
+	
+	//making space for matrix c
+	keyc = ftok(".",5983);
+	shmBuf3id = shmget(keyc,sizeof(int[10][10]),IPC_CREAT|0666);
+	if (shmBuf3id == -1) {
+		perror("shmget c error");
+		exit(1);
+	}	
+	shmptrc = shmat(shmBuf3id,0,0);
+	if (shmptrc == (void*) -1) {
+		perror("shmat c error");
+		exit(1);
+	}
+	printf("all shmatting and shmgetting are successful.\n");
+	
+	//put matrices into shm areas
+
+	//turning id's to strings for argument passing
+	sprintf(strida, "%d", shmBuf1id);
+	sprintf(stridb, "%d", shmBuf2id);
+	sprintf(stridc, "%d", shmBuf3id);	
+	
+	/*
+ * 	argv[0] = "./multiply";
+ * 	argv[1] = strida;
+ * 	argv[2] = stridb;
+ * 	argv[3] = stridc;
+ *	argv[6] = acolnum;
+ * 	
+ 
+	for (int i = 0; i < arownum; i++) {
+		for (int j = 0; j < bcolnum; j++) {
+			argv[4] = i;
+			argv[5] = j;
+		}
+	}
+	*/
+
+	pid = fork();
+	if (pid == 0) {
+		printf("I'm a child. [0][0] is %d\n",shmptra[0][0]);
+		//must exec and reconnect to memory
+		//must pass shmBuf1id as parameter 
+		argv[0] = "./multiply";
+		argv[1] = strida;
+		argv[2] = stridb;
+		argv[3] = stridc;
+		//argv[4] = i;
+		//argv[5] = j;
+		//argv[6] = acolnum;
+		execvp("./multiply",argv);
+		//printf("I'm supposedly running multiply with exec\n");
+	}
+	else wait(NULL);
+	//for size of arrays, how big should they be initialized?
+	//then read matrix c and print it out
+}
 
 int main (void) {
 	char buffer [80];
-	int mata[10][10];
-	int matb[10][10];
-	int arowcount = 0;
-	int acolcount = 0;
-	int browcount = 0;
-	int bcolcount = 0;
 	int temp = 0;
 	int toput;
 	//state keeps track of whether we are currently at matrix A or matrix B
@@ -65,4 +175,7 @@ int main (void) {
 		}
 		printf("\n");
 	}
+
+	createBuf1();
+	/*************************/
 }
