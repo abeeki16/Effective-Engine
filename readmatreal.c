7ec502cab6e4da25fddea2f0c673ea8ffe889e91
User-PC\User <sollozzo@bu.edu>
2018-11-11 13:58:12 -0500

    readmatreal update

--- readmatreal.c
+++ readmatreal.c
@@ -4,10 +4,14 @@
 
 int main (void) {
 	char buffer [80];
+	int mata[10][10];
+	int matb[10][10];
 	int arowcount = 0;
 	int acolcount = 0;
 	int browcount = 0;
 	int bcolcount = 0;
+	int temp = 0;
+	int toput;
 	//state keeps track of whether we are currently at matrix A or matrix B
 	int state = 0;
 	char * current;
@@ -28,21 +32,37 @@ int main (void) {
 				if (arowcount == 0) {
 					acolcount++;
 				}
+				//change cur string to int to put into 
+				//temp is the current column number
+				sscanf(current, "%d", &toput);
+				mata[arowcount][temp] = toput;
 				//need to check if cur row is same as first row?
 			}
 			else if (state == 1) {
 				if (browcount == 0) bcolcount++;
+				sscanf(current, "%d", &toput);
+				matb[browcount][temp] = toput;
 			}
 			printf("%s",current);
 			current = strtok(NULL, " ");
+			temp++;
 		}
 		printf("\n");
 		if (state == 0) arowcount++;
 		else if (state == 1) browcount++;
+		temp = 0;
 	}
 	printf("arow = %d", arowcount);
 	printf("acol = %d", acolcount);
 	printf("brow = %d", browcount);
 	printf("bcol = %d\n", bcolcount);
-	if (acol != brow) printf("invalid matrices\n");
+	if (acolcount != browcount) printf("invalid matrices\n");
+
+	//print matrix a
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%d ",mata[i][j]);
+		}
+		printf("\n");
+	}
 }
