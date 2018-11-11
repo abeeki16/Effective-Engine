b20a27ec7cb54fbd36890568a2ff2f10439334b5
Chi Guang Lin <sollozzo@csa2.bu.edu>
2018-11-11 16:22:40 -0500

    updating givenshm

--- givenshm.c
+++ givenshm.c
@@ -7,10 +7,20 @@ key_t keya;
 int shmBuf1id;
 int (*shmptra)[10];
 
+key_t keyb;
+int shmBuf2id;
+int (*shmptrb)[10];
+
+key_t keyc;
+int shmBuf3id;
+int (*shmptrc)[10];
+
 void createBuf1() {	
-	char * argv[2];
+	char * argv[4];
 	//will need more strids for other matrices
 	char strida[20];
+	char stridb[20];
+	char stridc[20];
 	pid_t pid;
 	//making matrix a
 	keya = ftok(".",'a');
@@ -30,10 +40,40 @@ void createBuf1() {
 	printf("%d\n",shmptra[0][0]);
 	printf("%p\n",shmptra); */
 
+	//making space for matrix b
+	keyb = ftok(".",5982);
+	shmBuf2id = shmget(keyb,sizeof(int[10][10]),IPC_CREAT|0666);
+	if(shmBuf2id == -1) {
+		perror("shmget b error");
+		exit(1);
+	}
+	shmptrb = shmat(shmBuf2id,0,0);
+	if (shmptrb == (void*) -1) {
+		perror("shmat b error");
+		exit(1);
+	}
+	
+	//making space for matrix c
+	keyc = ftok(".",5983);
+	shmBuf3id = shmget(keyc,sizeof(int[10][10]),IPC_CREAT|0666);
+	if (shmBuf3id == -1) {
+		perror("shmget c error");
+		exit(1);
+	}	
+	shmptrc = shmat(shmBuf3id,0,0);
+	if (shmptrc == (void*) -1) {
+		perror("shmat c error");
+		exit(1);
+	}
+	printf("all shmatting and shmgetting are successful.\n");
+	
 	//put matrices into shm areas
 
-
+	//turning id's to strings for argument passing
 	sprintf(strida, "%d", shmBuf1id);
+	sprintf(stridb, "%d", shmBuf2id);
+	sprintf(stridc, "%d", shmBuf3id);	
+
 	pid = fork();
 	if (pid == 0) {
 		printf("I'm a child. [0][0] is %d\n",shmptra[0][0]);
@@ -41,12 +81,14 @@ void createBuf1() {
 		//must pass shmBuf1id as parameter 
 		argv[0] = "./multiply";
 		argv[1] = strida;
+		argv[2] = stridb;
+		argv[3] = stridc;
 		execvp("./multiply",argv);
 		//printf("I'm supposedly running multiply with exec\n");
 	}
 	else wait(NULL);
 	//for size of arrays, how big should they be initialized?
-
+	//then read matrix c and print it out
 }
 
 int main(int argc, int *argv[]) {
