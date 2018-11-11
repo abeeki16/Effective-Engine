768c28e1fee808e7112d02ffda0c7aab61b03cb3
Chi Guang Lin <sollozzo@csa2.bu.edu>
2018-11-11 16:19:01 -0500

    update multiply

--- multiply.c
+++ multiply.c
@@ -7,18 +7,31 @@
 
 int main(int argc, char * argv[]) {
 	//turn argv[1] back to a pointer again
-	int (*shmpointer)[9];
-	char buffer[20];
-	int shmid;
-	
-	strcpy(buffer,argv[1]);
+	int (*shmpointera)[10];
+	int (*shmpointerb)[10];
+	int (*shmpointerc)[10];
+	int shmida,shmidb,shmidc;
+	int arowind, bcolind, acolnum;
+	int sum = 0;
+	//acolnum is equal to brownum
 
-	printf("exec: argv0 is %s, argv1 is %s\n",argv[0],argv[1]);
-	sscanf(buffer, "%d", &shmid);	
-	printf("exec: shmid is %d\n", shmid);
-	shmpointer = shmat(shmid, 0, 0);
+	//strcpy(buffer,argv[1]);
+	//printf("exec: argv0 is %s, argv1 is %s\n",argv[0],argv[1]);
 	
-	printf("pointer is %p\n",shmpointer);
-	printf("Hi! I'm exec. [0][0] is %d\n",shmpointer[0][0]);
+	//turn args into ints for shmatting
+	sscanf(argv[1], "%d", &shmida);
+	sscanf(argv[2], "%d", &shmidb);
+	sscanf(argv[3], "%d", &shmidc);
+	printf("exec: shmids are %d %d %d\n", shmida, shmidb, shmidc);
+
+	//initialize pointers
+	shmpointera = shmat(shmida, 0, 0);
+	shmpointerb = shmat(shmidb, 0, 0);
+	shmpointerc = shmat(shmidc, 0, 0,);
+
+	
+	
+	printf("pointer is %p\n",shmpointera);
+	printf("Hi! I'm exec. [0][0] is %d\n",shmpointera[0][0]);
 	exit(EXIT_SUCCESS);
 }
