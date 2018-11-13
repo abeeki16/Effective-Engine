c4d31fe7e069b27b89250aac344bc18ecff5d297
User-PC\User <sollozzo@bu.edu>
2018-11-12 19:08:17 -0500

    adding transpose

---
+++ transpose.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+	char buffer[80];
+	int matrix[10][10];
+	int resultmatrix[10][10];
+	char * current;
+
+	int rowcount;
+	int colcount;
+	int temp = 0;
+	int toput;
+
+	while (fgets (buffer, 79 ,stdin) != NULL) {
+		buffer[strlen(buffer)-1] = '\0';
+		current = strtok(buffer, " ");
+		while (current != NULL) {
+			if (rowcount == 0) {
+				colcount++;
+			}
+			sscanf(current, "%d", &toput);
+			matrix[rowcount][temp] = toput;
+			current = strtok(NULL, " ");
+			temp++;
+		}
+		rowcount++;
+		temp = 0;
+	}
+	int i;
+	int j;
+	for (i = 0; i < rowcount; i++) {
+		for (j = 0; j < colcount; j++) {
+			resultmatrix[j][i] = matrix[i][j];
+		}
+	}
+
+	//print result
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
+			printf("%d ", resultmatrix[i][j]);
+		}
+		printf("\n");
+	}
+
+}
