e26ad9e9c5b037465d40065f70c573a85ad9ae1f
User-PC\User <sollozzo@bu.edu>
2018-11-14 18:30:41 -0500

    piping update

--- piping.c
+++ piping.c
@@ -6,6 +6,34 @@
 #include <string.h>
 
 int main(void) {
+	char input[80];
+	char * current;	
+	printf("myshell> ");
+	char * reverser[20];
+	int revpointer = 0;
+	//char * reversed[20];
+	//int tempcounter = 0;
+
+	fgets(input,80,stdin);
+	input[strlen(input)-1] = '\0';
+	current = strtok(input,"|");
+	
+	while (current != NULL) {
+		//subparser(current);
+		//printf("%s\n",current);
+		//TODO: make fork here. if current process is parent, skip the call to subparser.
+		reverser[revpointer] = current;
+		revpointer++;
+		current = strtok(NULL,"|");
+	}
+	for (int i = revpointer-1; i >= 0; i--) {
+		printf("%s\n",reverser[i]);
+		//reversed[tempcounter] = reverser[i];
+		//tempcounter++;
+	}
+
+	/************************** pipeshell ends here *************************************************/
+
 	int pipefd[2]; // pipefd[0]: read endpoint, pipefd[1]: write endpoint
     pid_t cpid;
     char buf;
@@ -20,7 +48,7 @@ int main(void) {
     if (cpid == 0) {
     	dup2(pipefd[1], 1);
     	//change stdout to pipefd write end
-    	exec("./toexec", (char *) NULL);
+    	exec("reverser[revpointer-1]", (char *) NULL);
     }
 
     //parent
@@ -30,6 +58,6 @@ int main(void) {
     	dup2(pipefd[0], 0);
     	close(pipefd[0]);
     	//change stdin to pipfd read end
-    	exec("./toexec2", (char *) NULL);
+    	exec("reverser[revpointer]", (char *) NULL);
     }
 }
