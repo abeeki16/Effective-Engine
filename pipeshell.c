d553051102d134e21d2cbaa261db462ebfc6929e
Ajit Beeki <abeeki20@csa2.bu.edu>
2018-11-14 22:41:41 -0500

    Updated pipeshell with recursion

--- pipeshell.c
+++ pipeshell.c
@@ -2,19 +2,58 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
-void pipeparser(char * command) {
+#include <unistd.h>
+static char * reverser[20];
+void pipeparser(int i, int rightPipe[2]) {
+	int leftPipe[2];
+	pid_t pid;
+	if (i==0) {
+		//pipe(rightPipe);
+		pid = fork();
+		if (pid==0) {
+			dup2(rightPipe[1],1);
+			close(rightPipe[0]);
+			close(rightPipe[1]);
+			execvp(reverser[i],NULL);
+		} 
+	} else if (rightPipe==NULL) {
+		pipe(leftPipe);
+		pid = fork();
+		if (pid ==0) {
+			dup2(leftPipe[0],0);
+			close(leftPipe[0]);
+			close(leftPipe[1]);
+			pipeparser(i-1,leftPipe);
+			execvp(reverser[i],NULL);
+		}
+	} else {
+		pipe(leftPipe);
+		pid = fork();
+		if(pid==0) {
+			dup2(rightPipe[1],1);
+			close(rightPipe[0]);
+			close(rightPipe[1]);
+			
+			dup2(leftPipe[0],0);
+			close(leftPipe[0]);
+			close(leftPipe[1]);
+			pipeparser(i-1,leftPipe);
+			execvp(reverser[i], (char *) NULL);
+		} 
+	} 
+	
 	
 }
 
 int main (void) {
 	char input[80];
 	char * current;	
+	
 	printf("myshell> ");
-	char * reverser[20];
+	
 	int revpointer = 0;
 	char reversed[250];
-
+	pid_t pid;
 	fgets(input,80,stdin);
 	input[strlen(input)-1] = '\0';
 	current = strtok(input,"|");
@@ -27,9 +66,11 @@ int main (void) {
 		revpointer++;
 		current = strtok(NULL,"|");
 	}
-	for (int i = revpointer-1; i >= 0; i--) {
+	
+	pipeparser(revpointer-1,NULL);
+	int i;
+	for (i = revpointer-1; i >= 0; i--) {
 		printf("%s\n",reverser[i]);
-		
 	}
 
 }
